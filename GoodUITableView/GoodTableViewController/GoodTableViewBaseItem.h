771f95b6c962beedd41417debb61714480864ee6
Chibaibuki <budingmail@vip.qq.com>
2016-04-24 20:44:58 +0800

    Create the Class-GoodTableViewItem

---
+++ GoodUITableView/GoodTableViewController/GoodTableViewBaseItem.h
@@ -0,0 +1,13 @@
+//
+//  GoodTableViewBaseItem.h
+//  GoodUITableView
+//
+//  Created by 高向孚 on 4/24/16.
+//  Copyright © 2016 ByStudio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GoodTableViewBaseItem : NSObject
+
+@end
---
+++ GoodUITableView/GoodTableViewController/GoodTableViewBaseItem.m
@@ -0,0 +1,13 @@
+//
+//  GoodTableViewBaseItem.m
+//  GoodUITableView
+//
+//  Created by 高向孚 on 4/24/16.
+//  Copyright © 2016 ByStudio. All rights reserved.
+//
+
+#import "GoodTableViewBaseItem.h"
+
+@implementation GoodTableViewBaseItem
+
+@end
